82103f6ec8b5fbe5a4800aff390fb2e4b9e5525e
Dima K <lusdjke@gmail.com>
2019-11-11 16:12:33 +0800

    Updates

---
+++ precache-manifest.d4e9d83d36b9846234c3de70cabe2e24.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4096e7bab8e30840d8b",
+    "url": "/social-network/static/js/runtime~main.d4096e7b.js"
+  },
+  {
+    "revision": "2be81fe3aa75abb6e181",
+    "url": "/social-network/static/css/main.f72f2fee.chunk.css"
+  },
+  {
+    "revision": "74ab3a4b65d04814e59a43543c8379f0",
+    "url": "/social-network/static/media/preloader.74ab3a4b.svg"
+  },
+  {
+    "revision": "1869630e4389de47986a",
+    "url": "/social-network/static/js/1.1869630e.chunk.js"
+  },
+  {
+    "revision": "e24d0aec88db38f8561a",
+    "url": "/social-network/static/js/2.e24d0aec.chunk.js"
+  },
+  {
+    "revision": "2be81fe3aa75abb6e181",
+    "url": "/social-network/static/js/main.2be81fe3.chunk.js"
+  },
+  {
+    "revision": "4fae85deab89f55362a90bf0d4a3ceca",
+    "url": "/social-network/static/media/headerlogo.4fae85de.png"
+  },
+  {
+    "revision": "c3b711703e1cd516ca6b171d6ad695fc",
+    "url": "/social-network/static/media/user.c3b71170.png"
+  },
+  {
+    "revision": "cce99c86028a3e00a0aa50eee2648055",
+    "url": "/social-network/static/media/banner.cce99c86.jpg"
+  },
+  {
+    "revision": "1869630e4389de47986a",
+    "url": "/social-network/static/css/1.3970a2e2.chunk.css"
+  },
+  {
+    "revision": "56d9b90a218028ae5c981d2f3f5a7021",
+    "url": "/social-network/index.html"
+  }
+];
